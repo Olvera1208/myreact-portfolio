ef5fd8789643d6158daa1e0e80444a5ae1ea8850
isiah Olvera <itolvera98@gmail.com>
2022-03-04 15:58:33 -0800

    imported new folder and added new file NavBar.js with code

---
+++ src/components/NavBar.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+function NavBar({ currentPage, handlePageChange }) {
+  return (
+    <header>
+      <h1 id="header">Isiah Olvera</h1>
+      <nav>
+        <ul className="nav nav-tabs justify-content-end">
+          <li className="nav-item">
+            <a
+              href="#About"
+              onClick={() => handlePageChange("About")}
+              className={currentPage === "AboutMe" ? "nav-link active" : "nav-link"
+              }
+            >
+              About 
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#Portfolio"
+              onClick={() => handlePageChange("Portfolio")}
+              className={currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+              }
+            >
+              Portfolio
+            </a>
+          </li>
+          <li id="nav-item">
+            <a
+              href="#Resume"
+              onClick={() => handlePageChange("Resume")}
+                className={currentPage === "Resume" ? "nav-link active" : "nav-link"
+              }
+            >
+            Resume
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              onClick={() => handlePageChange("contact")}
+                className={currentPage === "Contact" ? "nav-link active" : "nav-link"
+              }
+              >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
