27270a0432a66355ce91fa14dfa63efa7d8a8921
isiah Olvera <itolvera98@gmail.com>
2022-03-04 17:17:27 -0800

    added new folder and file aboutme with code

---
+++ src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+ function About() {
+  return (
+    <div>
+      <h1 className='text-center'>About Me</h1>
+      <p>
+        My name is Isiah Olvera. I was born in Rancho Mirage CA, I played sports all through out
+        my childhood all I knew was sports my childhood. After I graduated HighSchool I didn't really have a plan 
+        or career so I decided to go to College of the Desert here in Palm Desert CA. I attended for 2 years before i decided to call it quits.
+        I dropped out of school I just didn't want to learn anymore or take test.
+        This was my biggest challenge I didn't know what I wanted to do with my life. I didn't have 
+        a career or a plan for what I wanted to do. All I did was work and play video games everyday 
+        but i knew I needed to do something with my self. I talked to my family and told me about USCD 
+        coding bootcamp. I then decided to enroll and now im learning things I never knew before 
+        or heard of ever but i am enjoying it.
+      </p>
+    </div>
+  );
+}; 
+
+export default About;
