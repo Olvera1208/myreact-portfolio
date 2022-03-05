d2bbf676803db26e26f9e1dec1ef264df57a21f7
isiah Olvera <itolvera98@gmail.com>
2022-03-04 18:08:05 -0800

    Adding in place holder code

---
+++ src/components/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+    <h1>Contact</h1>
+      <p>
+        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
+        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
+        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
+        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
+        mi. In a molestie nisl, eu sodales diam.
+      </p>
+    </div>
+  );
+}
